9f448ccb940fdb632bd2d7a45dc578f812a6e217
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2014-12-04 16:40:07 +0000

    ipとuser_idの失敗回数を記録するためのテーブルを追加

---
+++ php/schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `login` varchar(255) NOT NULL UNIQUE,
+  `password_hash` varchar(255) NOT NULL,
+  `salt` varchar(255) NOT NULL
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `login_log` (
+  `id` bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `created_at` datetime NOT NULL,
+  `user_id` int,
+  `login` varchar(255) NOT NULL,
+  `ip` varchar(255) NOT NULL,
+  `succeeded` tinyint NOT NULL
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `ip_bans` (
+  `ip` varchar(255) NOT NULL PRIMARY KEY,
+  `num` int DEFAULT 0
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `user_locks` (
+  `user_id` int NOT NULL PRIMARY KEY,
+  `num` int DEFAULT 0
+) DEFAULT CHARSET=utf8;
